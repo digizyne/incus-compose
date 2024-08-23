dfc66750b08040c94026d04b4afcbf768fd3b46d
jfcastel0 <joshua.castellucci@digizyne.net>
2024-08-23 16:14:06 -0400

    Added volumes and container_name to up function. Need to tackle env vars next

--- up.go
+++ up.go
@@ -9,12 +9,24 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-func createService(incusComposeService IncusComposeService) {
-	serviceName := strings.Split(incusComposeService.Image, ":")[1]
+func createService(incusComposeService IncusComposeService, serviceName string) {
+	configurationOptions := []string{
+		"create",
+		incusComposeService.Image,
+		serviceName,
+	}
+	if len(incusComposeService.Environment) > 0 {
+		for _, env := range incusComposeService.Environment {
+			configurationOptions = append(configurationOptions, fmt.Sprintf("-c environment.%s", env))
+		}
+	}
+
+	fmt.Println(configurationOptions)
+
 	fmt.Println(Blue, "*** creating service", serviceName, "***")
 	createCommand := exec.Command("incus", "create", incusComposeService.Image, serviceName)
 	createOutput, err := createCommand.CombinedOutput()
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(Red, err)
 		return
 	}
@@ -27,7 +39,7 @@ func createProxy(incusProxy IncusProxy, serviceName string) {
 	fmt.Println(Blue, "*** creating proxy", incusProxy.Listen, incusProxy.Connect, "***")
 	proxyCommand := exec.Command("incus", "config", "device", "add", serviceName, fmt.Sprintf("%s-proxy-%s", serviceName, proxyPort), "proxy", fmt.Sprintf("listen=%s", incusProxy.Listen), fmt.Sprintf("connect=%s", incusProxy.Connect))
 	proxyOutput, err := proxyCommand.CombinedOutput()
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(Red, fmt.Sprint(err))
 		return
 	}
@@ -38,7 +50,7 @@ func createVolume(volumeName string) {
 	fmt.Println(Blue, "*** creating volume", volumeName, "***")
 	volumeCommand := exec.Command("incus", "storage", "volume", "create", "default", volumeName)
 	volumeOutput, err := volumeCommand.CombinedOutput()
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(Red, err)
 		return
 	}
@@ -47,37 +59,20 @@ func createVolume(volumeName string) {
 
 func mountVolume(volume IncusServiceVolume, serviceName string) {
 	fmt.Println(Blue, "*** mounting volume", volume.Source, "to", volume.Target, "***")
-	mountCommand := exec.Command("incus", "storage", "volume", "attach", "default", volume.Source, serviceName, volume.Target)
+	mountCommand := exec.Command("incus", "config", "device", "add", serviceName, fmt.Sprintf("%s-%s-disk", serviceName, volume.Source), "disk", "pool=default", fmt.Sprintf("source=%s", volume.Source), fmt.Sprintf("path=%s", volume.Target), fmt.Sprintf("readonly=%t", volume.Read_Only))
 	mountOutput, err := mountCommand.CombinedOutput()
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(Red, err)
 		return
 	}
 	fmt.Println(Green, string(mountOutput))
 }
 
-func bindMount(volume IncusServiceVolume, serviceName string) {
-	fmt.Println(Blue, "*** bind mounting directory", volume.Source, "to", volume.Target, "***")
-	bindMountCommand := exec.Command("incus", "file", "mount", fmt.Sprintf("%s/%s", serviceName, volume.Target), volume.Source)
-	// var stdout, stderr bytes.Buffer
-	bindMountCommand.Stdout = nil
-	bindMountCommand.Stderr = nil
-	bindMountCommand.Stdin = nil
-	// go func() {
-		err2 := bindMountCommand.Run()
-		if (err2 != nil) {
-			fmt.Println(Red, err2)
-			return
-		}
-	// }()
-	// fmt.Println(Green, string(bindMountOutput))
-}
-
 func startService(serviceName string) {
 	fmt.Println(Blue, "*** starting service", serviceName, "***")
 	launchCommand := exec.Command("incus", "start", serviceName)
 	launchOutput, err := launchCommand.CombinedOutput()
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(Red, err)
 		return
 	}
@@ -101,29 +96,33 @@ func up() {
 
 	//* Create volumes
 	for key := range incusCompose.Volumes {
-		if (!incusCompose.Volumes[key].External) {
+		if !incusCompose.Volumes[key].External {
 			createVolume(key)
 		}
 	}
 
 	//* Create services
 	for key := range incusCompose.Services {
-		createService(incusCompose.Services[key])
+		var serviceName string
+		if incusCompose.Services[key].Container_Name != "" {
+			serviceName = incusCompose.Services[key].Container_Name
+		} else {
+			serviceName = strings.Split(incusCompose.Services[key].Image, ":")[1]
+		}
+
+		createService(incusCompose.Services[key], serviceName)
 
 		fmt.Println(Blue, "*** configuring service", key, "***")
 		for _, proxy := range incusCompose.Services[key].Devices.Proxies {
-			createProxy(proxy, key)
+			createProxy(proxy, serviceName)
 		}
 
 		for _, volume := range incusCompose.Services[key].Volumes {
-			if (volume.Type == "volume") {
-				mountVolume(volume, key)
-			}
-			if (volume.Type == "bind") {
-				bindMount(volume, key)
+			if volume.Type == "volume" {
+				mountVolume(volume, serviceName)
 			}
 		}
 
-		startService(key)
+		startService(serviceName)
 	}
 }
